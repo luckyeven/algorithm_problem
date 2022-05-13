cccfd6f539483016044e598ef60c93060b54d968
luckyeven <ssong094@uottawa.ca>
2022-05-13 00:35:25 -0400

    update codes

--- findLocalMinimumNumber/localMin.java
+++ findLocalMinimumNumber/localMin.java
@@ -1,18 +1,21 @@
 /**
- * Given an array arr[0 .. n-1] of distinct integers, the task is to find a local minima in it.
- * We say that an element arr[x] is a local minimum if it is less than both its neighbors. 
+ * Given an array arr[0 .. n-1] of distinct integers, the task is to find a
+ * local minima in it.
+ * We say that an element arr[x] is a local minimum if it is less than both its
+ * neighbors.
  * for corner elements, we need to consider only one neighbor for comparison.
- * There can be more than one local minima in an array, we need to find one of them.
+ * There can be more than one local minima in an array, we need to find one of
+ * them.
  * 
  * Requirement: time complex O(logN)
  * 
- * Examples:   
+ * Examples:
  * 
  * Input: arr[] = {9, 6, 3, 14, 5, 7, 4};
  * Output: Index of local minima is 2
- * The output prints index of 3 because it is 
- * smaller than both of its neighbors. 
- * Note that indexes of elements 5 and 4 are 
+ * The output prints index of 3 because it is
+ * smaller than both of its neighbors.
+ * Note that indexes of elements 5 and 4 are
  * also valid outputs.
  * 
  * Input: arr[] = {23, 8, 15, 2, 3};
@@ -22,47 +25,46 @@
  * Output: Index of local minima is 0
  * 
  * Input: arr[] = {3, 2, 1};
- * Output: Index of local minima is 2 
+ * Output: Index of local minima is 2
  * 
  * @author Shifeng Song @luckyeven
- * @since  2022-05-06
- * @version 1.0
+ * @since 2022-05-06
+ * @version 2.0
  * 
  */
 
-
 public class LocalMin {
 
-  
     static final int MINIMUM_LENGTH_OF_ARRAY = 3;
 
-    public static int findAllLocalMin(int [] arr, int first, int last){
-        int mid =first + (last - first)/2;
+    public static int findLocalMin(int[] arr, int first, int last) {
+        int mid = first + (last - first) >> 1;
 
-        if(mid == 0){return mid;}
-
-        if(last - first < MINIMUM_LENGTH_OF_ARRAY){
-            if(arr[first]< arr[first+1]){return first;}
-            if(arr[last]< arr[last-1]){return last;}
-        }
-
-
-
-        if( arr[mid -1] > arr[mid] && arr[mid] <arr[mid+1] ){
+        if (mid == 0) {
             return mid;
-        }else if(arr[mid]< arr[mid-1]){
-            return findAllLocalMin(arr, mid+1, last );
         }
-        return findAllLocalMin(arr, first, mid+1);
+
+        if (last - first < MINIMUM_LENGTH_OF_ARRAY) {
+            if (arr[first] < arr[first + 1]) {
+                return first;
+            }
+            if (arr[last] < arr[last - 1]) {
+                return last;
+            }
+        }
+
+        if (arr[mid - 1] > arr[mid] && arr[mid] < arr[mid + 1]) {
+            return mid;
+        } else if (arr[mid] < arr[mid - 1]) {
+            return findLocalMin(arr, mid + 1, last);
+        }
+        return findLocalMin(arr, first, mid + 1);
     }
 
-    public static int findLocalMin(int[] arr, int l){
-        return findAllLocalMin(arr,0,l-1);
-    }
     public static void main(String[] args) {
-        int [] arr = {9, 6, 3, 14, 5, 7, 4};
-        int i = findLocalMin(arr, arr.length);
-        System.out.println("Index of local minima is "+ i );
+        int[] arr = { 9, 6, 3, 14, 5, 7, 4 };
+        int i = findLocalMin(arr, 0, arr.length - 1);
+        System.out.println("Index of local minima is " + i);
     }
 
 }
--- heap/KsortedArraySort.java
+++ heap/KsortedArraySort.java
@@ -13,27 +13,88 @@ import java.math.*;
 
 public class KsortedArraySort {
 
-    public static void heapInsert(int[] arr, int index){
-        
+    public static void heapSort(int[] arr, int k) {
+        if (arr == null) {
+            return;
+        }
+        int index = 0;
+        int[] heap = new int[k + 1];
+        int heapSize = k;
+
+        for (; index <= Math.min(arr.length, k); index++) {
+            heapInsert(heap, index, arr[index]);
+
+        }
+        int i = 0;
+        for (; index < arr.length; index++, i++) {
+            arr[i] = pop(heap, heapSize);
+            heapInsert(heap, heap.length - 1, arr[index]);
+        }
+
+        for (; i < arr.length; i++) {
+            arr[i] = pop(heap, heapSize--);
+        }
 
     }
 
-    public static void heapify(){
+    // make a small pile heap
+    public static void heapInsert(int[] arr, int index, int item) {
+        arr[index] = item;
+        while (arr[index] < arr[(index - 1) / 2]) {
+            swap(arr, (index - 1) / 2, index);
+            index = (index - 1) / 2;
+        }
+    }
 
+    // heapify the array starting at index
+    public static void heapify(int[] arr, int index, int heapSize) {
+        int leftLeaf = (index >> 1) + 1;
+        while (leftLeaf < heapSize) {
+            int smaller = leftLeaf + 1 < heapSize && arr[leftLeaf] > arr[leftLeaf + 1] ? leftLeaf + 1 : leftLeaf;
+
+            smaller = arr[index] < arr[smaller] ? index : smaller;
+
+            if (smaller == index) {
+                break;
+            }
+
+            swap(arr, index, smaller);
+            index = smaller;
+            leftLeaf = index * 2 + 1;
+        }
     }
 
     /**
-     * swap two number in the array 
-     * @param arr array to be sway 
-     * @param first first index of int number
-     * @param second second index of in number 
+     * swap two number in the array
+     * 
+     * @param arr    array to be sway
+     * @param first  first index of int number
+     * @param second second index of in number
      */
-    public static void swap(int [] arr, int first, int second){
-        if(arr[first]!= arr[second]){
-            arr[first] = arr[first] ^arr[second];
+    public static void swap(int[] arr, int first, int second) {
+        if (arr[first] != arr[second]) {
+            arr[first] = arr[first] ^ arr[second];
             arr[second] = arr[first] ^ arr[second];
             arr[first] = arr[first] ^ arr[second];
         }
+    }
+
+    /**
+     * pop up the first number in the heap
+     * 
+     * @param arr
+     * @return the first int number
+     */
+    public static int pop(int[] heap, int heapSize) {
+        if (heap == null) {
+            return -1;
+        }
+
+        swap(heap, 0, heapSize);
+        heapify(heap, 0, heapSize);
+        int number = heap[heapSize];
+
+        return number;
     }
 
     public static void printTree(int[] arr, int heapSize) {
@@ -67,6 +128,11 @@ public class KsortedArraySort {
     }
 
     public static void main(String[] args) {
-        
+        int[] arr = { 10, 9, 8, 7, 4, 70, 60, 50 }; // k = 4;
+        int k = 4;
+        System.out.println(Arrays.toString(arr));
+        heapSort(arr, k);
+        System.out.println(Arrays.toString(arr));
+
     }
 }
